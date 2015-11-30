267e00c0a801bcd1f711728e480ca31bc3cca9a6
Waseem <waseemm@loopindex.com>
2015-11-30 11:41:18 +0200

    add compenents

--- amirami/learn-angular/lib/js/controllers.js
+++ amirami/learn-angular/lib/js/controllers.js
@@ -3,7 +3,12 @@
  */
 
 var amirami = angular.module('amirami', []);
-amirami.controller('amiramiCtrl',function($scope){
-    console.log('wow')
+amirami.controller('amiramiCtrl', ['$scope','$http' ,function($scope, $http){
+    $http.get("http://localhost:63342/amirami/learn-angular/data.json")
+    .success(function(response) {
+            $scope.people = response;
+            $scope.peopleOrder = 'name';
+    });
+    //
 
-})
+}])
